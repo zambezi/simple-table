54aa1232ac90db47855c6ab9e9d1eaca1b9965e6
Gabriel Montagne <Gabriel.Montagne@rbs.com>
2016-07-20 12:07:04 +0100

    Add support from UMD and support maps

--- rollup.config.js
+++ rollup.config.js
@@ -3,5 +3,8 @@ import babel from 'rollup-plugin-babel'
 export default {
   entry: 'src/main.js',
   dest: 'dist/simple-table.js',
+  format: 'umd',
+  moduleName: 'simpleTable',
+  sourceMap: true,
   plugins: [ babel() ]
 }
