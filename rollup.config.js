6489fa7c1660b2d2b67010732dee89ce9922b264
Leon Coto <leon.coto@rbs.com>
2016-08-26 10:54:32 +0100

    Update build configuration

--- rollup.config.js
+++ rollup.config.js
@@ -1,5 +1,4 @@
 import babel from 'rollup-plugin-babel'
-import babelrc from 'babelrc-rollup'
 
 export default {
   entry: 'src/index.js',
@@ -8,7 +7,11 @@ export default {
   moduleName: 'simpleTable',
   external: [ '@zambezi/d3-rebind', 'd3-dispatch', 'd3-selection', 'underscore' ],
   sourceMap: true,
-  plugins: [ babel(babelrc()) ],
+  plugins: [ babel({
+    "presets": [["es2015", { "modules": false }]],
+    "plugins": ["external-helpers"],
+    "babelrc": false
+  }) ],
   globals: {
     '@zambezi/d3-rebind': 'd3',
     'd3-dispatch': 'd3',
